0b96e688779aca4d2d0392bb2abb3e67222322d4
ChristianCeT <chrisatnat@hotmail.com>
2023-04-27 15:36:16 -0500

    second commit

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            leading: const Padding(
+              padding: EdgeInsets.all(4.0),
+              child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://preview.redd.it/8uob81i6l7671.jpg?width=400&format=pjpg&auto=webp&v=enabled&s=8fe72809e58bce809326c43559888034aefc43ab")),
+            ),
+            title: const Text('Mi amor ❤️')),
+        body: _ChatView());
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller: chatProvider.chatScrollController,
+                itemCount: chatProvider.messageList.length,
+                itemBuilder: (context, index) {
+                  final message = chatProvider.messageList[index];
+
+                  return (message.fromWho == FromWho.hers)
+                      ? HerMessageBubble(message: message)
+                      : MyMessageBubble(message: message);
+                },
+              ),
+            ),
+            MessageFieldBox(
+              onValue: (value) => chatProvider.sendMessage(value),
+            ),
+            const SizedBox(height: 6),
+          ],
+        ),
+      ),
+    );
+  }
+}
